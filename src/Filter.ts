5e9a34390e581fda454682f41fce85fdc145dc39
ffTsuzuku <gagandeepsandhu@protonmail.com>
2022-05-22 19:04:45 -0400

    docs: Added in comments for the filter class

--- src/Filter.ts
+++ src/Filter.ts
@@ -7,6 +7,14 @@
  * private methods or classes if you like.
  */
 export default class Filter {
+    /**
+     * Find tweets written by a particular user.
+     * @param tweets a list of tweets with distinct ids, not modified by this method.
+     * @param username Twitter username, required to be a valid Twitter username as
+     * defined by Tweet.getAuthor()'s spec.
+     * @return all and only the tweets in the list whose author is username,
+     * in the same order as in the input list.
+     */
     static writtenBy(
         tweets: TweetInterface[],
         username: string
@@ -14,6 +22,13 @@ export default class Filter {
         throw Error('Implement me!')
     }
 
+    /**
+     * Find tweets that were sent during a particular timespan.
+     * @param tweets a list of tweets with distinct ids, not modified by this method.
+     * @param timespan timespan
+     * @return all and only the tweets in the list that were sent during the timespan,
+     * in the same order as in the input list.
+     */
     static inTimespan(
         tweets: TweetInterface[],
         timespan: TimespanInterface
